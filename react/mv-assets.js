b478588671b5e53d01b764ff444c87a16eb1a967
Shrivu Shankar <shrivu1122@gmail.com>
2020-11-27 17:06:03 -0600

    mv assets script

---
+++ react/mv-assets.js
@@ -0,0 +1,25 @@
+const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+
+glob('../react/dist/**/*.*', {}, (err, files) => {
+  if (err) {
+    console.warn(err);
+    return;
+  }
+  files.map((fn) => {
+    let newFn = path.join(
+      '../assets',
+      fn.replace('/', '__').replace('\\', '__').replace('..__react/dist/', ''),
+    );
+    fs.mkdir(path.dirname(newFn), { recursive: true }, (err) => {
+      if (err) {
+        console.warn(err);
+        return;
+      }
+      fs.rename(fn, newFn, (err) => {
+        if (err) console.warn(err);
+      });
+    });
+  });
+});
